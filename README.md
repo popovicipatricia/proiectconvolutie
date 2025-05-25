9b96834dff912329069448cea99a46ed5e29e9bc
Popovici Patricia <popovicipatricia2004@gmail.com>
2025-05-25 23:35:56 +0300

    Creat README, actualizat cod si requirements

---
+++ README.md
@@ -0,0 +1,26 @@
+# Proiect Python-Convoluția Semnalelor Sinusoidale
+
+## Descriere proiect
+
+- Acest proiect are ca scop generarea a două semnale sinusoidale definite de utilizator și calculul convoluției între acestea.
+- Proiectul permite vizualizarea grafică a celor două semnale originale și a semnalului rezultat în urma convoluției, facilitând înțelegerea conceptelor din procesarea semnalelor.
+
+## Funcționalități
+
+- Generarea semnalelor sinusoidale pe baza parametrilor: amplitudine, frecvență și fază.
+- Calculul convoluției dintre cele două semnale folosind biblioteca SciPy.
+- Afișarea grafică a semnalelor și a convoluției folosind Matplotlib.
+
+## Tehnologii
+
+- Python
+- NumPy
+- SciPy
+- Matplotlib
+
+## Echipa
+
+*Grupa:* 31-E7
+
+- MĂCIUCĂ M. ANDREEA-CORINA  
+- POPOVICI P. PATRICIA
--- proiectpractic.py
+++ proiectpractic.py
@@ -1,61 +1,64 @@
 import numpy as np
+import matplotlib.pyplot
+"""
+Proiect - Convoluția semnalelor sinusoidale
+Tema: D4-T2 | Convoluția semnalelor
+Echipa: 31-E7
+Studenți: MĂCIUCĂ M. ANDREEA-CORINA, POPOVICI P. PATRICIA
+
+Surse folosite:
+- https://numpy.org/doc/stable/reference/generated/numpy.sin.html
+- https://numpy.org/doc/stable/reference/generated/numpy.convolve.html
+- https://docs.scipy.org/doc/scipy/reference/signal.html
+- https://matplotlib.org/
+"""
+
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
-
+# Generează un semnal sinusoidal
 def genereaza_sinus(amplitudine, frecventa, faza, durata, rata_esantionare):
-    t = np.arange(0, durata, 1 / rata_esantionare)
+    t = np.linspace(0, durata, int(rata_esantionare * durata), endpoint=False)
     y = amplitudine * np.sin(2 * np.pi * frecventa * t + faza)
     return t, y
 
+# Citim parametrii de la utilizator
+amp1 = float(input("Amplitudine semnal 1: "))
+freq1 = float(input("Frecvență semnal 1 (Hz): "))
+fase1 = float(input("Fază semnal 1 (radiani): "))
 
-def convolutioneaza_semnale(semnal1, semnal2):
-    return np.convolve(semnal1, semnal2, mode='full')
+amp2 = float(input("Amplitudine semnal 2: "))
+freq2 = float(input("Frecvență semnal 2 (Hz): "))
+fase2 = float(input("Fază semnal 2 (radiani): "))
 
-def vizualizeaza_semnale(t1, y1, t2, y2, y_conv, rata_esantionare):
-    t_conv = np.arange(0, len(y_conv)) / rata_esantionare
-
-    plt.figure(figsize=(12, 8))
-
-    plt.subplot(3, 1, 1)
-    plt.plot(t1, y1, label='Semnal 1')
-    plt.title('Semnal 1')
-    plt.grid(True)
-
-    plt.subplot(3, 1, 2)
-    plt.plot(t2, y2, label='Semnal 2', color='orange')
-    plt.title('Semnal 2')
-    plt.grid(True)
-
-    plt.subplot(3, 1, 3)
-    plt.plot(t_conv, y_conv, label='Convoluție', color='green')
-    plt.title('Convoluția celor două semnale')
-    plt.grid(True)
-
-    plt.tight_layout()
-    plt.show()
-
-
-# =======================
-# Parametrii definiți de utilizator
-# =======================
-amplitudine1 = 1.0
-frecventa1 = 5  # Hz
-faza1 = 0  # rad
-durata1 = 1.0  # secunde
-
-amplitudine2 = 0.5
-frecventa2 = 3  # Hz
-faza2 = np.pi / 4
-durata2 = 1.0  # secunde
-
+durata = 2  # secunde
 rata_esantionare = 1000  # Hz
 
-# Generare semnale
-t1, y1 = genereaza_sinus(amplitudine1, frecventa1, faza1, durata1, rata_esantionare)
-t2, y2 = genereaza_sinus(amplitudine2, frecventa2, faza2, durata2, rata_esantionare)
+# Generăm semnalele
+t1, y1 = genereaza_sinus(amp1, freq1, fase1, durata, rata_esantionare)
+t2, y2 = genereaza_sinus(amp2, freq2, fase2, durata, rata_esantionare)
 
-# Convoluție
-y_conv = convolutioneaza_semnale(y1, y2)
+# Calculăm convoluția
+convolutie = signal.convolve(y1, y2, mode='full')
+t_conv = np.linspace(0, 2 * durata, len(convolutie), endpoint=False)
 
-# Vizualizare
-vizualizeaza_semnale(t1, y1, t2, y2, y_conv, rata_esantionare)
+# Afișăm semnalele și convoluția
+plt.figure(figsize=(12, 8))
+
+plt.subplot(3, 1, 1)
+plt.plot(t1, y1, color="blue")
+plt.title("Semnal 1")
+plt.grid(True)
+
+plt.subplot(3, 1, 2)
+plt.plot(t2, y2, color="green")
+plt.title("Semnal 2")
+plt.grid(True)
+plt.subplot(3, 1, 3)
+plt.plot(t_conv, convolutie, color="red")
+plt.title("Convoluția")
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
